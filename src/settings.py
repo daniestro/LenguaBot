b8f561bfcf620f5a5bcb91aa6611dbff0e12f32a
Danila Kalganov <dkalganov@team.amocrm.com>
2024-11-27 22:24:57 +0100

    feat: add settings for postgres

--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,4 @@
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -6,4 +7,20 @@ class BotSettings(BaseSettings):
     model_config = SettingsConfigDict(env_prefix='BOT_')
 
 
+class PostgresqlSettings(BaseSettings):
+    DB: str
+    USER: str
+    PASSWORD: str
+    HOST: str = "postgresql"
+    PORT: str = "5432"
+    DRIVER: str = "postgresql+asyncpg"
+    model_config = SettingsConfigDict(env_prefix='POSTGRES_')
+
+
+class PostgresFullSettings(BaseModel):
+    postgres: PostgresqlSettings = PostgresqlSettings()
+    url: str = f"{postgres.DRIVER}://{postgres.USER}:{postgres.PASSWORD}@{postgres.HOST}:{postgres.PORT}/{postgres.DB}"
+
+
 bot_settings = BotSettings()
+postgres_settings = PostgresFullSettings()
