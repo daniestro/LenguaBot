210f12dabf02141d838c0f3e863aa9d2e6ba5f3b
Danila Kalganov <dkalganov@team.amocrm.com>
2024-12-27 15:21:29 +0100

    fix: add html converting

--- worker/telegrambot.py
+++ worker/telegrambot.py
@@ -5,7 +5,7 @@ from settings import bot_settings
 
 async def send_message(message: dict) -> None:
     url = f'https://api.telegram.org/bot{bot_settings.TOKEN}/sendMessage'
-    data = {'chat_id': message['chat_id'], 'text': f'Maybe u want to practise with word {message['word']}'}
+    data = {'chat_id': message['chat_id'], 'text': message['message'], 'parse_mode': 'html'}
     async with aiohttp.ClientSession() as session:
         async with session.post(url, data=data) as response:
             return await response.json()
