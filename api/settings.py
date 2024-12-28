636e75eaca0064ad78360d7c1d828b1d7583a302
Danila Kalganov <dkalganov@team.amocrm.com>
2024-12-28 22:44:48 +0100

    feat: database connection

--- api/settings.py
+++ api/settings.py
@@ -1,4 +1,5 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import BaseModel
 
 
 class JWTSettings(BaseSettings):
@@ -7,4 +8,21 @@ class JWTSettings(BaseSettings):
     model_config = SettingsConfigDict(env_prefix='API_JWT_')
 
 
+class PostgresqlSettings(BaseSettings):
+    DB: str
+    USER: str
+    PASSWORD: str
+    HOST: str = "postgresql"
+    PORT: str = "5432"
+    DRIVER: str = "postgresql+asyncpg"
+    model_config = SettingsConfigDict(env_prefix='POSTGRES_')
+
+
+class PostgresFullSettings(BaseModel):
+    postgres: PostgresqlSettings = PostgresqlSettings()
+    url: str = f"{postgres.DRIVER}://{postgres.USER}:{postgres.PASSWORD}@{postgres.HOST}:{postgres.PORT}/{postgres.DB}"
+
+
 jwt_settings = JWTSettings()
+postgres_settings = PostgresFullSettings()
+
