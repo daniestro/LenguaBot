a79bda2133afe6cb74ca2399187bb49747bace7b
Danila Kalganov <dkalganov@team.amocrm.com>
2025-01-09 00:21:54 +0100

    feat: change services for basic

--- api/services/roles.py
+++ api/services/roles.py
@@ -1,37 +1,16 @@
-from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import Depends
 
 from models import Roles
 from connectors.database import get_session
+from .base import OpenBaseService
 
 
-class RolesService:
-
-    def __init__(self, session: AsyncSession) -> None:
-        self.session = session
-
-    async def create(self, name: str) -> Roles:
-        role = Roles(name=name)
-        self.session.add(role)
-        await self.session.commit()
-        return role
-
-    async def get(self, _id: int) -> Roles:
-        query = select(Roles).where(Roles.id == _id)
-        response = await self.session.execute(query)
-        role = response.scalar_one_or_none()
-        return role
-
-    async def get_all(self, page: int, page_size: int) -> Roles:
-        offset = (page - 1) * page_size
-        query = select(Roles).offset(offset).limit(page_size)
-        result = await self.session.execute(query)
-        roles = result.scalars().all()
-        return roles
+class RolesService(OpenBaseService):
+    pass
 
 
 async def get_roles_service(
         session: AsyncSession = Depends(get_session)
 ) -> RolesService:
-    return RolesService(session)
+    return RolesService(session, Roles)
--- api/services/users.py
+++ api/services/users.py
@@ -1,57 +1,34 @@
-from typing import Optional, Sequence
-from uuid import UUID
-
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
 from sqlalchemy.orm import selectinload
 
 from connectors.database import get_session
+from .base import BaseService
 from models import Users
 
 
-class UserService:
+class UserService(BaseService):
 
-    def __init__(self, session: AsyncSession) -> None:
-        self.session = session
+    async def create(self, **kwargs) -> Users:
+        login = kwargs['login']
+        if await self._user_doesnt_exist(login):
+            return await self._add(**kwargs)
 
-    async def create(self, fullname: str, login: str, password: str) -> Users:
-        if await self.user_doesnt_exist(login):
-            user = Users(
-                fullname=fullname,
-                login=login,
-                password=password
-            )
-            self.session.add(user)
-            await self.session.commit()
-            return user
-
-    async def user_doesnt_exist(self, login: str) -> bool:
-        user = await self.get_by(login)
+    async def _user_doesnt_exist(self, login: str) -> bool:
+        user = await self.get(login=login)
         return False if user else True
 
-    async def get_by(self, login: str) -> Optional[Users]:
-        query = select(Users).where(Users.login == login)
-        return await self._execute(query)
+    async def get(self, **kwargs):
+        option = None
+        if 'id' in kwargs.keys():
+            option = selectinload(self.model.roles)
+        return await self._get(option, **kwargs)
 
-    async def get(self, _id: UUID | str):
-        query = select(Users).options(selectinload(Users.roles)).where(Users.id == _id)
-        return await self._execute(query)
-
-    async def get_all(self, page: int, page_size: int) -> Sequence[Users]:
-        offset = (page - 1) * page_size
-        query = select(Users).offset(offset).limit(page_size)
-        result = await self.session.execute(query)
-        users = result.scalars().all()
-        return users
-
-    async def _execute(self, query):
-        response = await self.session.execute(query)
-        user = response.scalar_one_or_none()
-        return user
+    async def all(self, *args):
+        return await self._all(args)
 
 
 async def get_user_service(
         session: AsyncSession = Depends(get_session)
 ) -> UserService:
-    return UserService(session)
+    return UserService(session, Users)
