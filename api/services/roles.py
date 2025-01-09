88ab9c305505d695c54a22f208bfdf216e1feb45
Danila Kalganov <dkalganov@team.amocrm.com>
2025-01-09 01:17:53 +0100

    fix: make RolesService great again

--- api/services/roles.py
+++ api/services/roles.py
@@ -3,11 +3,17 @@ from fastapi import Depends
 
 from models import Roles
 from connectors.database import get_session
-from .base import OpenBaseService
+from .base import Interfaces, BaseService
 
 
-class RolesService(OpenBaseService):
-    pass
+class RolesService(
+    BaseService,
+    Interfaces.Add,
+    Interfaces.Get,
+    Interfaces.All
+):
+    def __str__(self):
+        return "Service for managing roles"
 
 
 async def get_roles_service(
